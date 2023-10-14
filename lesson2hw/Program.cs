da3093851f5e4ced31ed6e448ae7f1bc0aa81ed4
PavlovDmitriy <feelerok@gmail.com>
2023-10-14 17:49:56 +0500

    add sort

--- lesson2hw/Program.cs
+++ lesson2hw/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace lesson2hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort.sortInject();
+            
+            int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+            int[] tempArr = new int[a.GetLength(0)*a.GetLength(1)];
+            int k =0;
+            for (int i=0; i<a.GetLength(0); i++)
+            {
+                for (int j=0; j<a.GetLength(1); j++)
+                {
+
+                    tempArr[k] = a[i,j];
+                    k++;
+                    Console.Write(a[i, j]+"\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("!!!!!!!!!!!!");
+            for (int i = 0; i < tempArr.Length; i++)
+            {
+                Console.Write(tempArr[i] + " ");
+            }
+            Console.WriteLine("Next>>>");
+            Sort.mergeSort(tempArr, 0, tempArr.Length - 1);
+            for (int i=0;i<tempArr.Length;i++)
+            {
+                Console.Write(tempArr[i]+" ");
+            }
+        }
+    }
+}
---
+++ lesson2hw/Sort.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2hw
+{
+    public class Sort
+    {
+        public static void mergeSort(int[] list, int start, int end)
+        {
+            int mid;
+            if (start < end)
+            {
+
+                mid = (start + end) / 2;
+                mergeSort(list, start, mid);
+                mergeSort(list, mid + 1, end);
+                merge(list, start, end, mid);
+            }
+        }
+
+        private static void merge(int[] list, int start, int end, int mid)
+        {
+            int[] mergedList = new int[list.Length];
+            int i, j, k;
+            i = start;
+            k = start;
+            j = mid + 1;
+
+            while (i <= mid && j <= end)
+            {
+                if (list[i] < list[j])
+                {
+                    mergedList[k] = list[i];
+                    k++;
+                    i++;
+                }
+                else
+                {
+                    mergedList[k] = list[j];
+                    k++;
+                    j++;
+                }
+            }
+
+            while (i <= mid)
+            {
+                mergedList[k] = list[i];
+                k++;
+                i++;
+            }
+
+            while (j <= end)
+            {
+                mergedList[k] = list[j];
+                k++;
+                j++;
+            }
+
+            for (i = start; i < k; i++)
+            {
+                list[i] = mergedList[i];
+            }
+        }
+
+        
+    }
+}
